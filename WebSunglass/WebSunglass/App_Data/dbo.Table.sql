7adc9879bf3957a4638ea9229abd91f067bfc6bd
Harelazimtas <48510673+Harelazimtas@users.noreply.github.com>
2019-09-09 12:20:35 +0300

    Add files via upload

---
+++ WebSunglass/WebSunglass/App_Data/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	[IdCustomer] INT NOT NULL PRIMARY KEY, 
+    [Id] INT NOT NULL, 
+    [FirstName] NCHAR(10) NOT NULL, 
+    [LastName] NCHAR(10) NOT NULL, 
+    [Email] NCHAR(10) NOT NULL
+)
