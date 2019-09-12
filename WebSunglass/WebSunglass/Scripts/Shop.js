b7f8dfd15dee529f7e08ef96c1accdbdf77af17d
Harelazimtas <48510673+Harelazimtas@users.noreply.github.com>
2019-09-12 12:44:53 +0300

    Add files via upload

---
+++ WebSunglass/WebSunglass/Scripts/Shop.js
@@ -0,0 +1,15 @@
+﻿
+
+function alertAddProduct() {
+    alert("Product successfully added");
+}
+
+function bigImg(x) {
+    x.style.height = "150px";
+    x.style.width = "200x";
+}
+
+function normalImg(x) {
+    x.style.height = "120px";
+    x.style.width = "120px";
+}
