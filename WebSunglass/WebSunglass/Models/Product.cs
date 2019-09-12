26fd2e616e8b0bc02f54aa8b96a3cae12e2aaba4
Harelazimtas <48510673+Harelazimtas@users.noreply.github.com>
2019-09-12 12:49:32 +0300

    Add files via upload

---
+++ WebSunglass/WebSunglass/Models/Product.cs
@@ -0,0 +1,46 @@
+﻿using DataLibary.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace WebSunglass.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Price { get; set; }
+        public String Name { get; set; }
+
+        public String Type { get; set;}
+         
+        public String Image { get; set;}
+
+
+        public static List<Product> LoadProduct(int Id)
+        {
+            string sql = @"select Id, ProductId, Price, Name, Image, Type from dbo.Product where Id="+Id;
+            return SqlDataAccess.LoadData<Product>(sql);
+        }
+
+        public static List<Product> LoadOtherProduct(int Id)
+        {
+            string sql = @"select Id, ProductId, Price, Name, Image, Type from dbo.Product where Id!=" + Id;
+            return SqlDataAccess.LoadData<Product>(sql);
+        }
+
+        public int InsertProduct()
+        {
+            
+            String sql = @"insert into dbo.Product (Id, Price, Name, Image, Type) values (@Id, @Price, @Name, @Image, @Type);";
+            return SqlDataAccess.SaveData(sql, this);
+        }
+
+    }
+}
