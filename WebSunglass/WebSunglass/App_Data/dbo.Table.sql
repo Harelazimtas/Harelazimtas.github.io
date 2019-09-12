be10bbf5b76855cf2a924bba994f05311f5308e6
Harelazimtas <48510673+Harelazimtas@users.noreply.github.com>
2019-09-12 12:52:54 +0300

    Delete dbo.Table.sql

--- WebSunglass/WebSunglass/App_Data/dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Customer]
-(
-	[IdCustomer] INT NOT NULL PRIMARY KEY, 
-    [Id] INT NOT NULL, 
-    [FirstName] NCHAR(10) NOT NULL, 
-    [LastName] NCHAR(10) NOT NULL, 
-    [Email] NCHAR(10) NOT NULL
-)
